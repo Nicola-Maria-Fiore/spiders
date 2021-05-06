6a59a262f620d1d91f1bbde3b6abd68a64b6a3de
Vito Ferrulli <vitof970@gmail.com>
2021-05-07 00:34:44 +0200

    implemented base master-worker pattern

--- Worker.py
+++ Worker.py
@@ -2,5 +2,12 @@ import pandas as pd
 
 class Worker:
 
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, jobs, wid):
+        self.jobs = jobs
+        self.wid = wid
+
+
+    def start(self):
+        print("Worker {}".format(str(self.wid)))
+        for j in self.jobs:
+            print(j[0])
