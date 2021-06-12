8e25aa8f1d5c0799337872fbc9b79d492bfd2e85
Vito Ferrulli <vitof970@gmail.com>
2021-06-12 09:55:07 +0200

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     print("start")
     if len(sys.argv)>2 and sys.argv[1]=="-monitor":
         monitor_taks = lambda : Monitor.start(int(sys.argv[2]))
-        schedule.every(5).seconds.do(monitor_taks)
+        #schedule.every(5).seconds.do(monitor_taks)
         schedule.every().day.at("00:00").do(monitor_taks)
         while True:
             schedule.run_pending()
